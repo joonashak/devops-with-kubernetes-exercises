75f6ff4bad214f1a2a761536f103c41382fd2890
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-03-01 15:02:19 +0200

    ex-1.05

---
+++ project/server/deploy.sh
@@ -0,0 +1,3 @@
+docker build -t dwk-project-server .
+docker tag dwk-project-server joonashak/dwk-project-server
+docker push joonashak/dwk-project-server
--- project/server/index.js
+++ project/server/index.js
@@ -2,4 +2,6 @@ const express = require('express')
 const server = express()
 const port = 3000
 
+server.get('*', (req, res) => res.send('Hello, world!'))
+
 server.listen(port, () => console.log(`Server started in port ${port}`))
