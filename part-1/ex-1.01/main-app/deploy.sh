4c73c6787d496e9ae7bdc3a1ffc0467ae9fae0ca
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-03-01 18:18:02 +0200

    ex-1.07

---
+++ part-1/ex-1.01/main-app/deploy.sh
@@ -0,0 +1,3 @@
+docker build -t dwk-main-app .
+docker tag dwk-main-app joonashak/dwk-main-app
+docker push joonashak/dwk-main-app
--- part-1/ex-1.01/main-app/index.js
+++ part-1/ex-1.01/main-app/index.js
@@ -1,5 +1,11 @@
+const express = require("express");
 const { generate } = require("randomstring");
 
-const s = generate()
+const s = generate();
 
-setInterval(() => console.log(`${new Date()} ${s}`), 5000)
+setInterval(() => console.log(`${new Date()} ${s}`), 5000);
+
+const server = express();
+server.get("*", (req, res) => res.send(`${new Date()} ${s}`));
+
+server.listen(3000);
