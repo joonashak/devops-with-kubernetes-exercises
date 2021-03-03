9a1440f9bf7517e3c876cab6a37a4251ccc1f3e9
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-03-04 00:41:54 +0200

    ex-2.06

--- part-1/ex-1.01/main-app-server/index.js
+++ part-1/ex-1.01/main-app-server/index.js
@@ -9,7 +9,7 @@ const server = express();
 server.get("*", async (req, res) => {
   const timestamp = fs.readFileSync("/app/files/timestamp");
   const result = await axios.get("http://ping-pong-svc:2345/n");
-  res.send(`${timestamp} ${s}<br />Ping / Pongs: ${result.data}`);
+  res.send(`${process.env.MESSAGE}<br />${timestamp} ${s}<br />Ping / Pongs: ${result.data}`);
 });
 
 server.listen(3000);
