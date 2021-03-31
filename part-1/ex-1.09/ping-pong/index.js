92c7c6313ef2c619b5b7b1736929ab998511dff4
Joonas Häkkinen <joonas@joonashakkinen.fi>
2021-03-31 23:47:46 +0300

    Working connection from app to db

--- part-1/ex-1.09/ping-pong/index.js
+++ part-1/ex-1.09/ping-pong/index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const { Client } = require("pg");
+
+const db = new Client();
+db.connect()
+  .then(() => console.log("connected"))
+  .catch((e) => console.log("not connected", e));
 
 let x = 0;
 
 const server = express();
+console.log(Object.keys(process.env));
 
-server.get('/n', (req, res) => res.send(`${x}`))
+server.get("/n", (req, res) => res.send(`${x}`));
 
 server.get("*", (req, res) => {
   x++;
@@ -12,3 +19,5 @@ server.get("*", (req, res) => {
 });
 
 server.listen(3000);
+
+//await db.end();
